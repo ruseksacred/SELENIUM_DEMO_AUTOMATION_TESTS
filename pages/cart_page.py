b360fb2900fbb47a6328f063d7fc29fb5d763002
Bartosz Rustyn <brustyn123@gmail.com>
2022-10-29 12:24:58 +0200

    added cart_xpath

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,7 +7,9 @@ from selenium.webdriver.common.by import By
 class CartPage:
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     return_to_shop_button_xpath = "//p[@class='return-to-shop']//a"
+    cart_title_text_xpath = "//div[@class='entry-header-inner']//h1"
+
 
     def click_return_to_shop_button(self):
         self.driver.find_element(By.XPATH, CartPage.return_to_shop_button_xpath).click()
-        
+
