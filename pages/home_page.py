f1fb14185bb0d971f1c0d07eccf27fb5b12baa5b
Bartosz Rustyn <brustyn123@gmail.com>
2022-11-01 08:49:46 +0100

    test_shop_button

--- pages/home_page.py
+++ pages/home_page.py
@@ -12,6 +12,7 @@ class HomePage:
     cart_tab_xpath = "//li[@id='menu-item-20']//a"
     shop_tab_xpath = "//li[@id='menu-item-21']//a"
     my_account_tab_xpath = "//li[@id='menu-item-22']//a"
+    shop_button_xpath = "//div[@class='sek-module-inner']//a"
 
     def click_cart_tab(self):
         self.driver.find_element(By.XPATH, HomePage.cart_tab_xpath).click()
@@ -21,4 +22,8 @@ class HomePage:
 
     def click_my_account_tab(self):
         self.driver.find_element(By.XPATH, HomePage.my_account_tab_xpath).click()
+
+    def click_shop_button(self):
+        self.driver.find_element(By.XPATH, HomePage.shop_button_xpath).click()
+
 
